5974d4b5c7dbf374a2dcf417612438099bfd13c9
wwan13 <wwan13@naver.com>
2024-05-09 16:25:54 +0900

    feat : init spring boot project

---
+++ src/main/java/io/wwan13/wintersecurity/WinterSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.wwan13.wintersecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/wwan13/wintersecurity/WinterSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wwan13.wintersecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
